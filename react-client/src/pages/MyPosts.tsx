dd644d1b1583f5aae0a31dd6c207b6ed0b4466b8
Sujitha Bayya <bayyasujitha.95@gmail.com>
2023-02-13 22:49:38 -0500

    Add post changes

--- react-client/src/pages/MyPosts.tsx
+++ react-client/src/pages/MyPosts.tsx
@@ -1,9 +1,11 @@
 import DOMPurify from "dompurify";
+import {Button} from "react-bootstrap";
 
 const MyPosts = () => {
   return (
     <div className="MyPosts">
       <h1>My Posts</h1>
+      <Button >Create Post</Button>
     </div>
   );
 };
