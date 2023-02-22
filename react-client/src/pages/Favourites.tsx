8698794b9cfd264263e73af094368c17ba86c244
unsafemango <unsafemango@gmail.com>
2023-02-21 21:51:41 -0500

    added favs and profile

--- react-client/src/pages/Favourites.tsx
+++ react-client/src/pages/Favourites.tsx
@@ -1,7 +1,39 @@
+import { AiFillStar } from "react-icons/ai";
+
 const Favourites = () => {
   return (
     <div className="Favourites">
       <h1>Favourites</h1>
+
+      <div className="content">
+        <div className="Post">
+          <h1 className="Post-title">Title</h1>
+          <p className="Post-username">by userA at 9:23pm</p>
+          <hr />
+          <p className="Post-tags">fantasy</p>
+          <p className="Post-content">This is just dummy data</p>
+          <div className="buttons">
+            {/* add like comment and favourites button based on authentication */}
+            <span role="button">
+              <AiFillStar className="btn-fav" />
+            </span>
+          </div>
+        </div>
+
+        <div className="Post">
+          <h1 className="Post-title">Title Title Title</h1>
+          <p className="Post-username">by userA at 9:23pm</p>
+          <hr />
+          <p className="Post-tags">fantasy</p>
+          <p className="Post-content">This is just dummy data</p>
+          <div className="buttons">
+            {/* add like comment and favourites button based on authentication */}
+            <span role="button">
+              <AiFillStar className="btn-fav" />
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
