491b9148d5de6edda7aaffa71a2c02c4be5982ca
muditdahiya <mdtheroxtar@gmail.com>
2023-02-21 20:49:09 -0500

    beautification

--- react-client/src/components/Post.tsx
+++ react-client/src/components/Post.tsx
@@ -1,5 +1,3 @@
-import DOMPurify from "dompurify";
-
 interface Props {
   title: string;
   content: string;
@@ -12,13 +10,17 @@ const Post = (post: Props) => {
   let date: string = post.date ? post.date.toString() : "";
 
   return (
-    <div className="post">
-      <h1 className="post-title">{post.title}</h1>
-      <br />
-      <p className="post-username">{post.username}</p>
-      <p className="post-date">{date}</p>
-      <p className="post-tags">{post.tags}</p>
-      <p className="post-content">{post.content}</p>
+    <div className="Post">
+      <h1 className="Post-title">{post.title}</h1>
+      <p className="Post-username">
+        by {post.username} at {date}
+      </p>
+      <hr />
+      <p className="Post-tags">{post.tags}</p>
+      <p className="Post-content">{post.content}</p>
+      <div className="buttons">
+        {/* add like comment and favourites button based on authentication */}
+      </div>
     </div>
   );
 };
